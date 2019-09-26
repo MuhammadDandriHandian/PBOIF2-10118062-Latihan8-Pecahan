618493b14a055534a1ab6bd554aed8f4e2ef931a
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-09-26 14:38:21 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan8-Pecahan/src/pboif2/pkg10118062/latihan8/pecahan/PBOIF210118062Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * Nama     : Muhammad Dandri Handian
+ * Kelas    : IF-2
+ * NIM      : 10118062
+ */
+public class PBOIF210118062Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
